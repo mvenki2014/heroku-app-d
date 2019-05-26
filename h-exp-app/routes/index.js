4eb7613deb695a0dd472d0328c0ffe96015a954c
mvenki2014 <mvneki2014@gmailcom>
2019-05-26 17:22:53 +0530

    changed database name

--- h-exp-app/routes/index.js
+++ h-exp-app/routes/index.js
@@ -5,14 +5,16 @@ var {Pool, Client} = require('pg');
 const pool = new Pool({
     user: 'rtdgwuicrlspms',
     host: 'ec2-54-197-239-115.compute-1.amazonaws.com',
-    database: 'd9abmah4kjopuk',
+    database: 'postgresql-reticulated-12689',
+    // database: 'd9abmah4kjopuk',
     password: '361e7db96571053d67a3ac4319610b4e9177c663f844f2a9ca576a1fa7c84e61',
     port: 5432,
 });
 const db = new Client({
     user: 'rtdgwuicrlspms',
     host: 'ec2-54-197-239-115.compute-1.amazonaws.com',
-    database: 'd9abmah4kjopuk',
+    database: 'postgresql-reticulated-12689',
+    // database: 'd9abmah4kjopuk',
     password: '361e7db96571053d67a3ac4319610b4e9177c663f844f2a9ca576a1fa7c84e61',
     port: 5432,
 });
