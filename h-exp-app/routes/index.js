b8cb5f87b66a8ba39547350f6e01543d785e2621
mvenki2014 <mvneki2014@gmailcom>
2019-05-26 12:10:43 +0530

    Fixed errors on Routes GET and POST Requests

--- h-exp-app/routes/index.js
+++ h-exp-app/routes/index.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  req.send({type:"GET",data:'Welcome to heroku nodejs by GET Request'})
+router.get('/', function (req, res, next) {
+    res.setHeader('content-type', 'application/json');
+    res.send({type: "GET", data: 'Welcome to heroku nodejs by GET Request'})
 });
 
-router.post('/', function(req, res, next) {
-  req.send({type:"post",data:'Welcome to heroku nodejs by POST Request'})
+router.post('/', function (req, res, next) {
+    res.setHeader('content-type', 'application/json');
+    res.send({type: "post", data: 'Welcome to heroku nodejs by POST Request'})
 });
 module.exports = router;
