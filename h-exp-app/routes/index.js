5dec33a12840b06eddda9ca568e5e5d455bad911
mvenki2014 <mvneki2014@gmailcom>
2019-05-26 17:04:04 +0530

    database connect with heroku database

--- h-exp-app/routes/index.js
+++ h-exp-app/routes/index.js
@@ -3,17 +3,17 @@ var router = express.Router();
 var {Pool, Client} = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'root',
+    user: 'rtdgwuicrlspms',
+    host: 'ec2-54-197-239-115.compute-1.amazonaws.com',
+    database: 'd9abmah4kjopuk',
+    password: '361e7db96571053d67a3ac4319610b4e9177c663f844f2a9ca576a1fa7c84e61',
     port: 5432,
 });
 const db = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'root',
+    user: 'rtdgwuicrlspms',
+    host: 'ec2-54-197-239-115.compute-1.amazonaws.com',
+    database: 'd9abmah4kjopuk',
+    password: '361e7db96571053d67a3ac4319610b4e9177c663f844f2a9ca576a1fa7c84e61',
     port: 5432,
 });
 db.connect();
