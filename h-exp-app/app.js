20d34cc802800fd5e2d60b2fc06f2789c2e209a1
mvenki2014 <mvneki2014@gmailcom>
2019-05-27 00:00:02 +0530

    File uploading error by using data encrption method

--- h-exp-app/app.js
+++ h-exp-app/app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var busboy = require('connect-busboy'); //middleware for form/file upload
+var fs = require('fs-extra');       //File System - for file manipulation
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
+app.use(busboy());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- h-exp-app/routes/index.js
+++ h-exp-app/routes/index.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 var {Client} = require('pg');
 var nodemailer = require('nodemailer');
+var path = require('path');     //used for file path
+var fs = require('fs-extra');       //File System - for file manipulation
+var busboy = require('connect-busboy'); //middleware for form/file upload
 
-const client = new Client({
+/*const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true,
-});
+});*/
 
-/*
 const client = new Client({
     user: 'postgres',
     host: 'localhost',
@@ -16,7 +18,6 @@ const client = new Client({
     password: 'root',
     port: 5432,
 });
-*/
 
 var transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -28,6 +29,23 @@ var transporter = nodemailer.createTransport({
         pass: 'Venkisingle123?'
     }
 });
+
+function uploadFile(req, res) {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename);
+
+        //Path where image will be uploaded
+        fstream = fs.createWriteStream('./public/uploads/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+            console.log("Upload Finished of " + filename);
+            //res.redirect('/thanks.html');           //where to go next
+        });
+    });
+}
+
 
 function sendmail(req, res) {
     var mailOptions = {
@@ -63,6 +81,7 @@ router.get('/getuser', function (req, res) {
     });
 });
 router.post('/postuser', function (req, res) {
+    console.log(req.body);
     if (req.body.submit === 'submit') {
         client.query("INSERT INTO reg_users (name, email, phone, status) VALUES ($1, $2, $3, 'Active') RETURNING id",
             [req.body.f_name + ' ' + req.body.l_name, req.body.email, req.body.phone], (err, dbRes) => {
@@ -74,9 +93,9 @@ router.post('/postuser', function (req, res) {
                         status: 'Success',
                         msg: 'row inserted with id: ' + dbRes.rows[0].id,
                     });*/
+                    uploadFile(req, res);
                     sendmail(req, res);
                 }
-
             });
     } else {
         res.send({
