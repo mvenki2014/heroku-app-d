5f19d14cdef3d9348f8548c3ce3773337250590e
mvenki2014 <mvneki2014@gmailcom>
2019-05-26 12:06:08 +0530

    Added new Routes GET and POST Requests

--- h-exp-app/routes/index.js
+++ h-exp-app/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  req.send({type:"GET",data:'Welcome to heroku nodejs by GET Request'})
 });
 
+router.post('/', function(req, res, next) {
+  req.send({type:"post",data:'Welcome to heroku nodejs by POST Request'})
+});
 module.exports = router;
